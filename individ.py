f8853d1a4306616bfa2ff1bf402e60b6acc4bc5e
justPasxa <lar.pasha.chenko@gmail.com>
2018-01-19 16:16:07 +0500

    Рефактор. Ренейминг имен.

--- individ.py
+++ individ.py
@@ -5,31 +5,30 @@ class Individual:
 
     def __init__(self, list_chromosome):
 
-        self.chromosomes = list_chromosome
-        self.correct_chromosomes = []
+        self.list_correct_chromosomes = []
         self.genotype = ""
         self.fit = 0
         self.count_correct_chromosome = 0
 
-        self._search_correct_chromosome()
-        self._create_genotype()
+        self._search_correct_chromosome(list_chromosome)
+        self._create_genotype(list_chromosome)
 
-    def _search_correct_chromosome(self):
-        for i in range(len(self.chromosomes)):
+    def _search_correct_chromosome(self, list_chromosome):
+        for i in range(len(list_chromosome)):
             check = True
-            for j in range(len(self.chromosomes)):
-                if self.chromosomes[j] != self.chromosomes[i] \
+            for j in range(len(list_chromosome)):
+                if list_chromosome[j] != list_chromosome[i] \
                         and i != j \
-                        and fabs(int(self.chromosomes[j], 2) - int(self.chromosomes[i], 2)) == fabs(j - i):
+                        and fabs(int(list_chromosome[j], 2) - int(list_chromosome[i], 2)) == fabs(j - i):
                     check = False
                     break
-                elif self.chromosomes[j] == self.chromosomes[i] and i != j:
+                elif list_chromosome[j] == list_chromosome[i] and i != j:
                     check = False
                     break
             if check:
                 self.count_correct_chromosome = self.count_correct_chromosome + 1
-                self.correct_chromosomes.append(self.chromosomes[i])
+                self.list_correct_chromosomes.append(list_chromosome[i])
 
-    def _create_genotype(self):
-        for i in range(len(self.chromosomes)):
-            self.genotype = self.genotype + self.chromosomes[i]
+    def _create_genotype(self, list_chromosome):
+        for i in range(len(list_chromosome)):
+            self.genotype = self.genotype + list_chromosome[i]
--- nqueens.py
+++ nqueens.py
@@ -38,7 +38,7 @@ class Solver_8_queens:
         '''
             Вывод
         '''
-        visualization = self.create_visualization(pop[self.pop_size - 1].correct_chromosomes)
+        visualization = self.create_visualization(pop[self.pop_size - 1].list_correct_chromosomes)
         best_fit = min_fitness
         epoch_num = count_epochs
 
@@ -50,37 +50,37 @@ class Solver_8_queens:
     def create_pop(self):
         pop = []
         for i in range(self.pop_size):
-            individ_list = ['000', '001', '010', '011', '100', '101', '110', '111']
-            random.shuffle(individ_list)
-            pop.append(individ.Individual(individ_list))
+            list_chromosome = ['000', '001', '010', '011', '100', '101', '110', '111']
+            random.shuffle(list_chromosome)
+            pop.append(individ.Individual(list_chromosome))
         return pop
 
     def search_fit(self, pop):
-        summ_correcet_chromosome = 0
+        sum_correct_chromosomes = 0
         for j in range(len(pop)):
-            summ_correcet_chromosome = summ_correcet_chromosome + pop[j].count_correct_chromosome
+            sum_correct_chromosomes = sum_correct_chromosomes + pop[j].count_correct_chromosome
         for i in range(len(pop)):
-            pop[i].fit = pop[i].count_correct_chromosome / summ_correcet_chromosome
+            pop[i].fit = pop[i].count_correct_chromosome / sum_correct_chromosomes
 
     def create_roulette(self, pop):
-        list_rulet = []
-        roulette_fit_start = 0
+        roulette = []
+        part_roulette_end = 0
         for i in range(len(pop)):
-            roulette_fit_end = roulette_fit_start
-            roulette_fit_start = roulette_fit_start + pop[i].fit
-            list_rulet.append((pop[i], (roulette_fit_end, roulette_fit_start)))
+            part_roulette_start = part_roulette_end
+            part_roulette_end = part_roulette_end + pop[i].fit
+            roulette.append((pop[i], (part_roulette_start, part_roulette_end)))
 
-        return self.twist_roulette(list_rulet)
+        return self.twist_roulette(roulette)
 
-    def twist_roulette(self, list_rulet):
-        selected_pop = []
+    def twist_roulette(self, roulette):
+        choosed_individuals = []
         for k in range(self.pop_size):
-            select = random.random()
-            for i in range(len(list_rulet)):
-                if (select >= list_rulet[i][1][0]) and (select < list_rulet[i][1][1]):
-                    selected_pop.append(list_rulet[i][0])
+            roulette_selection = random.random()
+            for i in range(len(roulette)):
+                if (roulette_selection >= roulette[i][1][0]) and (roulette_selection < roulette[i][1][1]):
+                    choosed_individuals.append(roulette[i][0])
                     break
-        return selected_pop
+        return choosed_individuals
 
     def to_crossing_over(self, selected_pop):
         childs = []
